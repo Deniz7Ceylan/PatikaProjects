0ba82bb24e79e4c48b938042956e2ebc33cfe852
Deniz7Ceylan <102260908+Deniz7Ceylan@users.noreply.github.com>
2023-02-15 01:03:43 +0300

    Add files via upload

---
+++ KarakterTerstenYazdirmaProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir metin girin: ");
+            string input = Console.ReadLine();
+            string output = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+
+            Console.WriteLine("Ters çevrilmiş metin: " + output);
+            Console.ReadLine();
+        }
+    }
+}
