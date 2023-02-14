25af78f9fd976be8f6f96af96b923e183c40efc5
Deniz7Ceylan <102260908+Deniz7Ceylan@users.noreply.github.com>
2023-02-15 02:10:24 +0300

    Add files via upload

---
+++ SessizHarfProgram.cs
@@ -0,0 +1,32 @@
+﻿namespace OrtaSeviyeProjeler
+{
+    internal class KarakterDegistirmeProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin girin: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+            string consonants = "bcdfghjklmnpqrstvwxyz";
+
+            foreach (string word in words)
+            {
+                bool hasConsecutiveConsonants = false;
+
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    if (consonants.Contains(word[i]) && consonants.Contains(word[i + 1]))
+                    {
+                        hasConsecutiveConsonants = true;
+                        break;
+                    }
+                }
+
+                Console.Write(hasConsecutiveConsonants + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
