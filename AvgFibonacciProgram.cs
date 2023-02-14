b5c712b1d003c26cf1b5c2a7b0fd16116e635184
Deniz7Ceylan <102260908+Deniz7Ceylan@users.noreply.github.com>
2023-02-15 00:31:31 +0300

    Add files via upload

---
+++ AvgFibonacciProgram.cs
@@ -0,0 +1,34 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int depth;
+
+            Console.Write("Fibonacci serisi derinliği: ");
+            depth = Convert.ToInt32(Console.ReadLine());
+
+            int[] fib = new int[depth];
+            fib[0] = 0;
+            if (depth > 1)
+            {
+                fib[1] = 1;
+            }
+
+            for (int i = 2; i < depth; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            double sum = 0;
+            for (int i = 0; i < depth; i++)
+            {
+                sum += fib[i];
+            }
+
+            double average = sum / depth;
+            Console.WriteLine("Fibonacci serisi ortalaması: " + average);
+        }
+    }
+}
